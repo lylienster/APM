64d9e10e7cf5dadfc2f4e43fa3d4995890144ec8
Tai Le <lylienster@gmail.com>
2016-10-09 15:11:41 -0500

    Setup the Http service

--- APM/app/app.component.ts
+++ APM/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from 'angular2/core';
 
+import { HTTP_PROVIDDERS } from "angular2/http";
+import "rxjs/Rx";   // load all features
 import { ProductListComponent } from './products/product-list.component';
 
 import { ProductService } from './products/product.service'; 
@@ -12,7 +14,7 @@ import { ProductService } from './products/product.service';
         </div>
     `,
     directives: [ProductListComponent],
-    providers: [ProductService]		
+    providers: [ProductService, HTTP_PROVIDDERS]		
 })
 export class AppComponent{
     pageTitle : string = "Acme Product Management";
