df4018fec961e9510708e52c9bf864dd5b5966fe
Tai Le <lylienster@gmail.com>
2016-10-09 20:42:53 -0500

    Subscribe to an Observable

--- APM/app/products/product-list.component.ts
+++ APM/app/products/product-list.component.ts
@@ -18,6 +18,7 @@ export class ProductListComponent implements OnInit{
     imageMargin : number = 2;
     showImage : boolean = false;
     listFilter : string;
+    errorMessage: string;
     products: IProduct[];
 
     constructor (private _productService: ProductService){
@@ -30,7 +31,11 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit(): void {
-        this.products = this._productService.getProducts();
+        this._productService.getProducts()
+                            .subscribe(
+                                products => this.products = products,
+                                error => this.errorMessage = <any>error
+                            );
     }
 
     onRatingClicked(message: string) : void {
