21965d0d9e77319667e85ebb8b30c642d2db0de5
Tai Le <lylienster@gmail.com>
2016-10-09 19:48:19 -0500

    Send Http request

--- APM/app/products/product.service.ts
+++ APM/app/products/product.service.ts
@@ -1,40 +1,24 @@
 import { Injectable } from "angular2/core";
 import { IProduct } from "./product";
+import { Http, Response } from "angular2/http";
+import { Observable } from "rxjs/Observable";
+
 
 @Injectable()
 export class ProductService{
+    private _productUrl = "api/products/products.json";
 
-    getProducts(): IProduct[]{
-        return [ 
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2016",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-        },
-        {
-            "productId": 8,
-            "productName": "Saw",
-            "productCode": "TBX-0022",
-            "releaseDate": "May 15, 2016",
-            "description": "15-inch steel blade hand saw",
-            "price": 11.55,
-            "starRating": 3.7,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
-        }];
+    constructor(private _http : Http){}
+
+    getProducts(): Observable<IProduct[]>: {
+        return this._http.get(this._productUrl)
+            .map((response: Response) => <IProduct[]>response.json())
+            .do(data => console.log("All: "+ JSON.stringifyu(data)))
+            .catch(this.handleError);
+    }
+
+    private handleError(error: Response){
+        console.error(error);
+        return Observable.throw(error.json().error || "Server error");
     }
 }
